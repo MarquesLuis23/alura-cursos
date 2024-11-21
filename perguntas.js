957e57f92de1d08edf3d68c09b64e95a25a0a457
MarquesLuis23 <00001101722812sp@al.educacao.sp.gov.br>
2024-11-20 21:37:25 -0300

    Projeto Final

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao( 
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação versátil e fácil de aprender, amplamente usada para desenvolvimento web, análise de dados e inteligência artificial.'
+);
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris, famosa por sua história, arte e a Torre Eiffel.'
+);
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz "oi" em inglês?',
+    'Dizemos "oi" em inglês como "Hi" ou "Hello".'
+);
+
+// Novas perguntas
+criaCartao(
+    'História',
+    'Quem foi Albert Einstein?',
+    'Albert Einstein foi um físico teórico, conhecido pela Teoria da Relatividade e pela famosa equação E=mc².'
+);
+
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Um número primo é aquele que só pode ser dividido por 1 e por ele mesmo, como 2, 3, 5, e 7.'
+);
+
+criaCartao(
+    'Ciências',
+    'Por que o céu é azul?',
+    'O céu é azul devido à dispersão da luz do sol pela atmosfera. A luz azul é espalhada mais que outras cores devido ao seu comprimento de onda menor.'
+);
+
+
